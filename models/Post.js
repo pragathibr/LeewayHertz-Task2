3630bf426c3d159ba3f85a9c8f6e873a39a4b2ed
Pragathi B R <43509523+pragathibr@users.noreply.github.com>
2020-08-16 00:08:39 +0530

    Add files via upload

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,27 @@
+const mongoose = require('mongoose');
 
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    lastname: {
+        type: String,
+        maxlength: 32,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        // unique: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Post',userSchema);
